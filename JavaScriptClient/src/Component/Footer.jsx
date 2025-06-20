94716b71442f4aadd2e5362893f696cbeab4241e
praveen choudhary <pc852180@gmail.com>
2025-06-20 14:25:40 +0530

    Crud Data

---
+++ JavaScriptClient/src/Component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <h1 style={{backgroundColor:"lightblue", textAlign:"center", fontSize:"30px", padding:'50px'}}>www.footer.com</h1>
+    
+    </>
+  )
+}
+
+export default Footer
