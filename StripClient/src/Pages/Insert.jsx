0db75ae3341e861931d22086fa311eac6157fbc4
praveen choudhary <pc852180@gmail.com>
2025-06-23 14:16:36 +0530

    Add to cart Stripe

--- StripClient/src/Pages/Insert.jsx
+++ StripClient/src/Pages/Insert.jsx
@@ -1,28 +1,69 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import BASE_URL from '../config';
 
 const Insert = ()=>{
+     
+    const [input, setInput] = useState("");
+    const [image, setImage]  =useState("");
+
+    const HandelInput = (e)=>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setInput(values=>({...values, [name]:value}));
+        console.log(input);
+    }
+
+    const HandelImage = (e)=>{
+        setImage(e.target.files);
+        console.log(image);
+    }
+
+    const HandelSubmit =async (e)=>{
+        e.preventDEfault();
+       
+    const formData = new FormData();
+    for(let key in input){
+        formData.append(key, input[key]);
+    }
+
+    for(let i = 0; i<=image.length;i++){
+        formData.append(image, image[i]);
+    }
+
+    const api = `${BASE_URL}/product/InsertProduct`;
+
+    const response = await  axios.post(api,formData);
+    console.log(response.data);
+    alert("Your data is insert")
+
+    }
+
+
     return(
         <>
         <div id="from">
         <h1>Registration Page</h1>
         <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Label>Enter the Product Name</Form.Label>
+        <Form.Control type="text" name='name' value={input.name} onChange={HandelInput} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Enter the Brand Name</Form.Label>
+        <Form.Control type="text" name='brand' value={input.brand} onChange={HandelInput} />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Enter the Price</Form.Label>
+        <Form.Control type="text" name='price' value={input.price} onChange={HandelInput} />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Label>Enter the Image Url</Form.Label>
+        <Form.Control type="file" multiple onChange={HandelImage}  />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={HandelSubmit}>
         Submit
       </Button>
     </Form>
