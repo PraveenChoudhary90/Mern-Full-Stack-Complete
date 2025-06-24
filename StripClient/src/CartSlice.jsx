a40dd9270bba3d2fdadb00310d02dc852051768b
praveen choudhary <pc852180@gmail.com>
2025-06-24 13:14:25 +0530

    Data Stripe

--- StripClient/src/CartSlice.jsx
+++ StripClient/src/CartSlice.jsx
@@ -14,14 +14,39 @@ const CartSlice = createSlice({
             else{
                 state.cart.push(actions.payload);
             }
-            
+        },
+        Increment:(state,actions)=>{
+            for(let i =0;i<state.cart.length;i++){
+                if(state.cart[i].id==actions.payload.id){
+                    state.cart[i].qnty++;
+                }
+            }
 
+        },
+        Decrement:(state, actions)=>{
+            for(let i =0;i<state.cart.length;i++){
+                if(state.cart[i].id==actions.payload.id)
+                    {
+                    if(state.cart[i].qnty<=1)
+                        {
+                        alert("You cant take less then one product")
+                    }
+                    else{
+                        state.cart[i].qnty--
+                    }
+                }
+            }
+            
+        },
+        RemoveItem:(state, actions)=>{
+            state.cart=state.cart.filter(key=>key.id != actions.payload.id);
         }
+
     }
 })
 
 
-export const {AddToCart} = CartSlice.actions;
+export const {AddToCart, Increment, Decrement,RemoveItem} = CartSlice.actions;
 export default CartSlice.reducer;
 
 
--- StripClient/src/Pages/CartData.jsx
+++ StripClient/src/Pages/CartData.jsx
@@ -1,9 +1,64 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import BASE_URL from "../config";
+import Table from 'react-bootstrap/Table';
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { Decrement, Increment, RemoveItem } from "../CartSlice";
 
 const CartData = ()=>{
+
+    const Product = useSelector(state=>state.mycart.cart);
+     console.log(Product);
+
+     const dispatch = useDispatch();
+
+ let totalAmount = 0;
+    const ans =Product.map(key=>{
+        totalAmount+=key.price * key.qnty;
+        return(
+            <>
+            <tr>
+                <td>
+                    <img src={`${BASE_URL}/${key.defaultImage}`} alt="" width="100px" height="100px" />
+                </td>
+                <td>{key.name}</td>
+                <td>{key.brand}</td>
+                <td>{key.price}</td>
+                <td>
+                <FaMinus style={{fontSize:"20px", marginRight:"10px"}} onClick={()=>{dispatch(Decrement({id:key.id}))}} />
+                  {key.qnty}
+                  <FaPlus  style={{fontSize:"20px", marginLeft:"10px"}} onClick={()=>{dispatch(Increment({id:key.id}))}} />
+                </td>
+                <td>{key.price * key.qnty}</td>
+                <td>
+                <MdDelete style={{fontSize:"20px", margin:"auto"}} onClick={()=>{dispatch(RemoveItem({id:key.id}))}} />
+                </td>
+            </tr>
+            </>
+        )
+    })
+
     return(
         <>
         <h1>Cart Data</h1>
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Brand</th>
+          <th>Price</th>
+          <th>Qunatity</th>
+          <th>TotalAmount</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+       {ans}
+        </tbody>
+        </Table>
+
         </>
     )
 }
